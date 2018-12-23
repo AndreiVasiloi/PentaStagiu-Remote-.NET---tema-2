32b85cbb45d95253ce4bd109db81d685a5f8677d
Vasiloi <andrei_vasiloi@yahoo.com>
2018-12-23 11:52:42 +0200

    Add Tema 2

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. ToUpper()
+            Console.WriteLine("1. ToUpper");
+            Console.WriteLine();
+            string upper = "andrei vasiloi";
+            Console.WriteLine("Before");
+            Console.WriteLine();
+            Console.WriteLine(upper);
+            Console.WriteLine();
+            Console.WriteLine("After");
+            Console.WriteLine();
+            Console.WriteLine(upper.ToUpper());
+            Console.WriteLine();
+
+            Console.ReadLine();
+            //2. IsNullOrEmpty
+            Console.WriteLine("2. IsNullOrEmpty");
+            Console.WriteLine();
+            Console.WriteLine("Hello, what is your name?");
+            string name = Console.ReadLine();
+
+
+            if (string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("You have to tell me your name.");
+            }
+            else
+            {
+                Console.WriteLine("Hello {0}", name);
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+
+            //3. Replace
+            Console.WriteLine("3. Replace");
+            Console.WriteLine();
+            Console.WriteLine("Before");
+            Console.WriteLine();
+            const string laptop = "I have a laptop";
+            Console.WriteLine(laptop);
+            Console.WriteLine();
+            string computer = laptop.Replace("laptop", "computer ");
+            Console.WriteLine("After");
+            Console.WriteLine();
+            Console.WriteLine(computer);
+            Console.ReadLine();
+
+            //4. Concat
+            Console.WriteLine();
+            Console.WriteLine("4. Concat");
+            Console.WriteLine();
+            string firstName = "Andrei";
+            string lastName = "Vasiloi";
+            string fullName = string.Concat(firstName, " ", lastName);
+            Console.WriteLine("Before");
+            Console.WriteLine();
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
+            Console.WriteLine();
+            Console.WriteLine("After");
+            Console.WriteLine(fullName);
+            Console.WriteLine();
+            Console.ReadLine();
+
+            //5. StartsWith
+            Console.WriteLine("5. StartsWith");
+            Console.WriteLine();
+            string input = "Andrei";
+
+            if (input.StartsWith("A"))
+            {
+                Console.WriteLine("The string starts with A");
+            }
+            else
+            {
+                Console.WriteLine("The string don`t start with A");
+            }
+
+            Console.ReadLine();
+
+            //6. Trim
+            Console.WriteLine("6. Trim");
+            Console.WriteLine();
+            string st = "      Andrei Vasiloi       ";
+            Console.WriteLine("Before");
+            Console.WriteLine();
+            Console.WriteLine(st);
+            Console.WriteLine();
+            st = st.Trim();
+            Console.WriteLine("After");
+            Console.WriteLine();
+            Console.WriteLine(st);
+            Console.ReadLine();
+
+            //7. IndexOf
+            Console.WriteLine("7. IndexOf");
+            Console.WriteLine();
+            const string value = "Andrei Vasiloi";
+
+            if (value.IndexOf("Andrei") != -1)
+            {
+                Console.WriteLine("string contains Andrei!");
+            }
+            else
+            {
+                Console.WriteLine("string is not containing Andrei");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
--- Microsoft .NET tema 2/Program.cs
+++ Microsoft .NET tema 2/Program.cs
@@ -6,10 +6,73 @@ using System.Threading.Tasks;
 
 namespace Microsoft.NET_tema_2
 {
+    public enum Gender {
+        Male,
+        Female
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello {0}", name);
+            Console.WriteLine("Can you tell me your year of birth please?");
+            string year = Console.ReadLine();
+            Console.WriteLine("And the month?");
+            string month = Console.ReadLine();
+            Console.WriteLine("The day of birth?");
+            string day = Console.ReadLine();
+            Console.WriteLine("What gender you have (m/f)?");
+            string gender = Console.ReadLine();
+            gender = gender.ToLower();
+            Gender? nullableGender = null;
+            if (gender == "m")
+            {
+                nullableGender = Gender.Male;
+            }
+
+            if (gender == "f")
+            {
+                nullableGender = Gender.Female;
+            }
+
+
+            string dateOfBirth = year + " " + month + " " + day;
+
+            DateTime value = DateTime.Parse(dateOfBirth);
+            var now = DateTime.Now;
+
+            TimeSpan diff = now - value;
+            int age = (int)(diff.TotalDays / 365);
+
+            int ageOfRetirementMale = 65;
+            int ageOfRetirementFemale = 63;
+
+            if (nullableGender == null)
+            {
+                return;
+            }
+
+            int ageOfRetirement = nullableGender == Gender.Male ? ageOfRetirementMale : ageOfRetirementFemale;
+
+            if (age < ageOfRetirement)
+            {
+                Console.WriteLine("You have to work until retirement " + (ageOfRetirement - age) + " more years.");
+            }
+
+            if (age >= ageOfRetirement)
+            {
+                Console.WriteLine("You are retired!!!");
+            }
+
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
